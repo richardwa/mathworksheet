d5c5fcc7ce5b687897a1a89519d90627bb180ae6
Richard Wang <rwang83@gmail.com>
2019-04-09 00:49:46 -0400

    update framwork

--- render.js
+++ render.js
@@ -1,31 +1,50 @@
 window.funcMap = {};
 
 const jx = (() => {
-    const funcMapInverse = new Map();
-    let counter = 0;
-    return {
-        render: (f, ...args) => {
-            //reset references
-            window.funcMap = {};
-            funcMapInverse.clear();
-            counter = 0;
+  const funcMapInverse = new Map();
+  let counter = 0;
+  const _jx = {
+    render: (f, ...args) => {
+      // reset references
+      window.funcMap = {};
+      funcMapInverse.clear();
+      counter = 0;
 
-            //render 
-            const s = f(...args);
-            if (Array.isArray(s)) {
-                document.write(s.join(""));
-            } else {
-                document.write(s);
-            }
-        },
-        ref: (f) => {
-            if (!funcMapInverse.has(f)) {
-                const name = '_' + counter;
-                counter++;
-                funcMapInverse.set(f, name);
-                window.funcMap[name] = f;
-            }
-            return `window.funcMap['${funcMapInverse.get(f)}']()`;
-        }
-    }
+      // render
+      const s = f(...args);
+      document.write(s);
+    },
+    ref: (f) => {
+      if (!funcMapInverse.has(f)) {
+        const name = '_' + counter;
+        counter++;
+        funcMapInverse.set(f, name);
+        window.funcMap[name] = f;
+      }
+      return `window.funcMap['${funcMapInverse.get(f)}']()`;
+    },
+
+    // plugins/utils
+    getURLState: () => {
+      if (document.location.search) {
+        const[state] =
+            [
+              document.location.search.substring(1)
+            ].map(decodeURIComponent)
+                .map(JSON.parse);
+        return state;
+      }
+    },
+    setURLState: (state) => {
+      document.location = `${document.location}?${JSON.stringify(state)}`;
+    },
+    random: Math.random,
+    rand: (size) => {
+      const min = Math.pow(10, size - 1);
+      const max = Math.pow(10, size);
+      return Math.floor(_jx.random() * (max - min)) + min;
+    },
+    pickOne: (...op) => { return op[Math.floor(_jx.random() * op.length)]; }
+  };
+  return _jx;
 })();
